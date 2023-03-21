6cd56f0a2c763439514e2eb69128adc4ffdf7907
Chantha Voeun <cvoeun45@gmail.com>
2023-03-20 21:30:28 -0400

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+if (blocks > 42) {
+    return blocks - 42;
+} else {
+    return 42 -blocks;
+}
+}
+
+function distanceFromHqInFeet(feet){
+    distanceFromHqInBlocks(feet);
+    return distanceFromHqInBlocks(feet) *264;
+}
+
+function distanceTravelledInFeet(start, end){
+    if (start > end){
+        return (start - end) * 264;
+    } else {
+        return (end - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    let feet = distanceTravelledInFeet(start, destination);
+    if (feet <= 400) {
+        return 0;
+    } else if (feet > 400 && feet <= 2000){
+        return (feet - 400) * 0.02;
+    } else if (feet > 2000 && feet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+
